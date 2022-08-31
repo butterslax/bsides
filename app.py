7d3ba62f64ded598750127c6d9a66e30c21b32ca
Tyler Parrott <tparrott@gmail.com>
2022-08-31 12:47:16 -0300

    cdk-nag

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 
 import aws_cdk as cdk
 from aws_cdk import Aspects
+from cdk_nag import NIST80053R5Checks
 
 # from bsides import CONSTANTS
 from bsides.bsides_stack import BsidesStack
@@ -16,5 +17,6 @@ test_stack = BsidesStack(
     # env=cdk.Environment(account=CONSTANTS.ACCOUNT, region=CONSTANTS.REGION),
 )
 
-Aspects.of(test_stack).add(s3_encryption_aspect())
+# Aspects.of(test_stack).add(s3_encryption_aspect())
+Aspects.of(test_stack).add(NIST80053R5Checks())
 app.synth()
