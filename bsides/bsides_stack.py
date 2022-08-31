5631e3f544eb47446aa219ea7a36b4f72724deba
Tyler Parrott <tparrott@gmail.com>
2022-08-31 09:57:27 -0300

    I hate spacing

--- bsides/bsides_stack.py
+++ bsides/bsides_stack.py
@@ -7,12 +7,12 @@ class BsidesStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-	pipeline =  CodePipeline(self, "Pipeline", 
-                        pipeline_name="MyPipeline",
-                        synth=ShellStep("Synth", 
-                            input=CodePipelineSource.git_hub("butterslax/bsides.git", "main"),
-                            commands=["npm install -g aws-cdk", 
-                                "python -m pip install -r requirements.txt", 
-                                "cdk synth"]
+            pipeline =  CodePipeline(self, "Pipeline", 
+                            pipeline_name="MyPipeline",
+                            synth=ShellStep("Synth", 
+                                input=CodePipelineSource.git_hub("butterslax/bsides.git", "main"),
+                                commands=["npm install -g aws-cdk", 
+                                    "python -m pip install -r requirements.txt", 
+                                    "cdk synth"]
+                            )
                         )
-                    )
