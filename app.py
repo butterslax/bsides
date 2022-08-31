b903521af55401d97090712ca4d80db21f2c53ee
Tyler Parrott <tparrott@gmail.com>
2022-08-31 10:51:56 -0300

    CONSTANTS'D

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import os
 
 import aws_cdk as cdk
 from aws_cdk import Aspects
-from bsides import CONSTANTS
+
+# from bsides import CONSTANTS
 from bsides.bsides_stack import BsidesStack
 
 from bsides.aspects import s3_version_aspect, s3_encryption_aspect
@@ -12,7 +13,7 @@ app = cdk.App()
 BsidesStack(
     app,
     "BsidesStack",
-    env=cdk.Environment(account=CONSTANTS.ACCOUNT, region=CONSTANTS.REGION),
+    # env=cdk.Environment(account=CONSTANTS.ACCOUNT, region=CONSTANTS.REGION),
 )
 
 Aspects.of(BsidesStack).add(s3_version_aspect())
--- bsides/bsides_stack.py
+++ bsides/bsides_stack.py
@@ -2,7 +2,8 @@ import aws_cdk as cdk
 from aws_cdk import aws_s3 as s3
 from aws_cdk import aws_iam as iam
 from constructs import Construct
-import CONSTANTS
+
+# import CONSTANTS
 
 
 class BsidesStack(cdk.Stack):
@@ -15,10 +16,10 @@ class BsidesStack(cdk.Stack):
             bucket_name="typarro-bsides-test-bucket",
             block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
         )
-        s3Bucket.add_to_resource_policy(
-            iam.PolicyStatement(
-                actions=["s3:*"],
-                resources=[s3Bucket.arn_for_objects("*")],
-                principals=[iam.AccountPrincipal(account_id=CONSTANTS.ACCOUNT)],
-            )
-        )
+        # s3Bucket.add_to_resource_policy(
+        #    iam.PolicyStatement(
+        #        actions=["s3:*"],
+        #        resources=[s3Bucket.arn_for_objects("*")],
+        #        principals=[iam.AccountPrincipal(account_id=CONSTANTS.ACCOUNT)],
+        #    )
+        # )
