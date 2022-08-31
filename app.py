df160c7b67dd096f79b2f3e0978fcb1be119ad70
Tyler Parrott <tparrott@gmail.com>
2022-08-31 10:41:19 -0300

    Added versioning aspect

--- app.py
+++ app.py
@@ -2,13 +2,18 @@
 import os
 
 import aws_cdk as cdk
+from aws_cdk import Aspects
 from bsides import CONSTANTS
 from bsides.bsides_stack import BsidesStack
 
+from bsides.aspects import s3_version_aspect, s3_encryption_aspect
 
 app = cdk.App()
-BsidesStack(app, "BsidesStack",
+BsidesStack(
+    app,
+    "BsidesStack",
     env=cdk.Environment(account=CONSTANTS.ACCOUNT, region=CONSTANTS.REGION),
-    )
+)
 
+Aspects.of(BsidesStack).add(s3_version_aspect())
 app.synth()
